1fdddc82b04fdae2992d62a4bc0ff8596eed19dc
MrOrz <johnsonliang7@gmail.com>
2019-05-12 13:53:18 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.edc25dde85872a0179e88b1f2c8e50e4.js"
+  "/precache-manifest.fa82802b0f300e4910ab3c1610649267.js"
 );
 
 workbox.clientsClaim();
